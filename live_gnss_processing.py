affe2f575e3e2dc0b9f6c00a9691f73f811b3483
20shaked20 <shakedlevi32@gmail.com>
2024-06-29 19:30:05 +0300

    getting data for all sattelites using lignsc library + altering the code alot, dropped ephamersis_manager from older code

--- live_gnss_processing.py
+++ live_gnss_processing.py
@@ -12,7 +12,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import navpy
-from gnssutils import EphemerisManager
+from gnss_lib_py.utils.ephemeris_downloader import load_ephemeris
+import gnss_lib_py.utils.time_conversions as tc
+from gnss_lib_py.navdata.navdata import NavData
+
 
 pd.options.mode.chained_assignment = None
 
@@ -20,6 +23,8 @@ pd.options.mode.chained_assignment = None
 WEEKSEC = 604800
 LIGHTSPEED = 2.99792458e8
 GPS_EPOCH = datetime(1980, 1, 6, 0, 0, 0)
+MU = 3.986005e14  # Earth's universal gravitational parameter
+OMEGA_E_DOT = 7.2921151467e-5  # Earth's rotation rate
 
 
 "FETCH DATA USING ADB"
@@ -46,63 +51,39 @@ def read_existing_file(file_path):
 
 def delete_files_in_directory(directory):
     try:
-        # Get list of files in the directory
         files = get_files_in_directory(directory)
-        
         for file in files:
             file_to_delete = f'{directory}/{file}'
-            
-            # Delete the file
             delete_command = ['shell', 'rm', file_to_delete]
             run_adb_command(delete_command)
             print(f"Deleted {file_to_delete}")
-    
     except Exception as e:
         print(e)
 
 def pull_and_append_files(directory_to_pull, destination):
     try:
-        # Get list of files in the directory
         files = get_files_in_directory(directory_to_pull)
-        
         for file in files:
             file_to_pull = f'{directory_to_pull}/{file}'
             destination_file = f'{destination}{file}'
-            
-            # Pull the file content from the device
             pull_command = ['shell', 'cat', file_to_pull]
             new_data = run_adb_command(pull_command)
-            
-            # Read existing file content if it exists
             existing_data = read_existing_file(destination_file)
-            
-            # Find the new data to append
             if new_data.startswith(existing_data):
                 new_content_to_append = new_data[len(existing_data):]
             else:
                 new_content_to_append = new_data
-            
-            # Append new data if there's any new content
             if new_content_to_append:
                 append_new_data(destination_file, new_content_to_append)
                 print(f"Appended new data to {destination_file}")
                 process_file(destination_file)  # Process the new data
-            else:
-                # print(f"No new data to append for {destination_file}")
-                pass
-    
     except Exception as e:
         print(e)
 
-
-""""""""""""""""""""""""""""""""""""""""""""""""
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process GNSS log files for positioning.')
-
-    # add --data_directory argument
     parser.add_argument('--data_directory', type=str, help='Directory for ephemeris data', default=os.getcwd())
     args = parser.parse_args()
-
     return args
 
 def read_data(input_filepath):
@@ -124,9 +105,15 @@ def read_data(input_filepath):
 
 def preprocess_measurements(measurements):
     measurements.loc[measurements['Svid'].str.len() == 1, 'Svid'] = '0' + measurements['Svid']
-    measurements['Constellation'] = measurements['ConstellationType'].map({'1': 'G', '3': 'R'})
+    constellation_map = {
+        '1': 'G',  # GPS
+        '3': 'R',  # GLONASS
+        '5': 'E',  # Galileo
+        '6': 'C',  #Beido
+    }
+    measurements['Constellation'] = measurements['ConstellationType'].map(constellation_map)
     measurements['SvName'] = measurements['Constellation'] + measurements['Svid']
-    measurements = measurements[measurements['Constellation'] == 'G']
+    measurements = measurements[measurements['Constellation'].isin(constellation_map.values())]
     numeric_cols = ['Cn0DbHz', 'TimeNanos', 'FullBiasNanos', 'ReceivedSvTimeNanos',
                     'PseudorangeRateMetersPerSecond', 'ReceivedSvTimeUncertaintyNanos',
                     'BiasNanos', 'TimeOffsetNanos']
@@ -148,126 +135,118 @@ def preprocess_measurements(measurements):
     measurements['PrSigmaM'] = LIGHTSPEED * 1e-9 * measurements['ReceivedSvTimeUncertaintyNanos']
     return measurements
 
-def calculate_satellite_position(ephemeris, transmit_time):
-    mu = 3.986005e14
-    OmegaDot_e = 7.2921151467e-5
-    F = -4.442807633e-10
-    sv_position = pd.DataFrame()
-    sv_position['sv']= ephemeris.index
-    sv_position.set_index('sv', inplace=True)
-    sv_position['t_k'] = transmit_time - ephemeris['t_oe']
-    A = ephemeris['sqrtA'].pow(2)
-    n_0 = np.sqrt(mu / A.pow(3))
-    n = n_0 + ephemeris['deltaN']
-    M_k = ephemeris['M_0'] + n * sv_position['t_k']
-    E_k = M_k
-    err = pd.Series(data=[1]*len(sv_position.index))
-    i = 0
-    while err.abs().min() > 1e-8 and i < 10:
-        new_vals = M_k + ephemeris['e']*np.sin(E_k)
-        err = new_vals - E_k
-        E_k = new_vals
-        i += 1
-        
-    sinE_k = np.sin(E_k)
-    cosE_k = np.cos(E_k)
-    delT_r = F * ephemeris['e'].pow(ephemeris['sqrtA']) * sinE_k
-    delT_oc = transmit_time - ephemeris['t_oc']
-    sv_position['delT_sv'] = ephemeris['SVclockBias'] + ephemeris['SVclockDrift'] * delT_oc + ephemeris['SVclockDriftRate'] * delT_oc.pow(2)
 
-    v_k = np.arctan2(np.sqrt(1-ephemeris['e'].pow(2))*sinE_k,(cosE_k - ephemeris['e']))
+def calculate_satellite_position(rinex_nav, gps_time):
+    positions = []
+    for _, eph in rinex_nav.iterrows():
+        try:
+            a = eph['sqrtA'] ** 2
+            n0 = np.sqrt(MU / a ** 3)
+            n = n0 + eph['deltaN']
+            tk = gps_time - eph['t_oe']
+            mk = eph['M_0'] + n * tk
+            ek = mk
+            for _ in range(10):
+                ek = mk + eph['e'] * np.sin(ek)
+            vk = np.arctan2(np.sqrt(1 - eph['e'] ** 2) * np.sin(ek), np.cos(ek) - eph['e'])
+            phi_k = vk + eph['omega']
+            delta_uk = eph['C_us'] * np.sin(2 * phi_k) + eph['C_uc'] * np.cos(2 * phi_k)
+            delta_rk = eph['C_rs'] * np.sin(2 * phi_k) + eph['C_rc'] * np.cos(2 * phi_k)
+            delta_ik = eph['C_is'] * np.sin(2 * phi_k) + eph['C_ic'] * np.cos(2 * phi_k)
+            uk = phi_k + delta_uk
+            rk = a * (1 - eph['e'] * np.cos(ek)) + delta_rk
+            ik = eph['i_0'] + delta_ik + eph['IDOT'] * tk
+            xk_prime = rk * np.cos(uk)
+            yk_prime = rk * np.sin(uk)
+            omega_k = eph['Omega_0'] + (eph['OmegaDot'] - OMEGA_E_DOT) * tk - OMEGA_E_DOT * eph['t_oe']
+            xk = xk_prime * np.cos(omega_k) - yk_prime * np.sin(omega_k) * np.cos(ik)
+            yk = xk_prime * np.sin(omega_k) + yk_prime * np.cos(omega_k) * np.cos(ik)
+            zk = yk_prime * np.sin(ik)
+            positions.append((eph['sv_id'], xk, yk, zk))
+        except Exception as e:
+            # Handle the exception
+            pass
+    
+    result_df = pd.DataFrame(positions, columns=['sv_id', 'x_k', 'y_k', 'z_k'])
+    return result_df
 
-    Phi_k = v_k + ephemeris['omega']
-
-    sin2Phi_k = np.sin(2*Phi_k)
-    cos2Phi_k = np.cos(2*Phi_k)
-
-    du_k = ephemeris['C_us']*sin2Phi_k + ephemeris['C_uc']*cos2Phi_k
-    dr_k = ephemeris['C_rs']*sin2Phi_k + ephemeris['C_rc']*cos2Phi_k
-    di_k = ephemeris['C_is']*sin2Phi_k + ephemeris['C_ic']*cos2Phi_k
-
-    u_k = Phi_k + du_k
-
-    r_k = A*(1 - ephemeris['e']*np.cos(E_k)) + dr_k
-
-    i_k = ephemeris['i_0'] + di_k + ephemeris['IDOT']*sv_position['t_k']
-
-    x_k_prime = r_k*np.cos(u_k)
-    y_k_prime = r_k*np.sin(u_k)
-
-    Omega_k = ephemeris['Omega_0'] + (ephemeris['OmegaDot'] - OmegaDot_e)*sv_position['t_k'] - OmegaDot_e*ephemeris['t_oe']
-
-    sv_position['x_k'] = x_k_prime*np.cos(Omega_k) - y_k_prime*np.cos(i_k)*np.sin(Omega_k)
-    sv_position['y_k'] = x_k_prime*np.sin(Omega_k) + y_k_prime*np.cos(i_k)*np.cos(Omega_k)
-    sv_position['z_k'] = y_k_prime*np.sin(i_k)
-    return sv_position
+from gnss_lib_py.parsers.rinex_nav import RinexNav
+import re
 
 def process_file(file_path):
     args = parse_arguments()
     try:
         unparsed_measurements = read_data(file_path)
         measurements = preprocess_measurements(unparsed_measurements)
-        print(args.data_directory)
-        manager = EphemerisManager(args.data_directory)
+        # print(f"Data Directory: {args.data_directory}")
+        
+
+        ## TODO: fix this to get the data from the datacdenter, its having issues with C import
+        
+        gps_millis = measurements['GpsTimeNanos'].values / 1e6  # Convert nanoseconds to milliseconds
+
+        # Check and print the min and max gps_millis to ensure they are after January 1, 2013
+        min_gps_millis = np.min(gps_millis)
+        max_gps_millis = np.max(gps_millis)
+        # print(f"Min gps_millis: {min_gps_millis}, Max gps_millis: {max_gps_millis}")
+
+        ephemeris_files = load_ephemeris('rinex_nav', gps_millis.astype(np.int64), constellations=['gps'], download_directory=args.data_directory, verbose=True)
+        # print(f"Ephemeris files loaded: {ephemeris_files}")
+        
+        # parser = RINEXParser("C:/Users/shake/OneDrive/Desktop/VsCode/GNSS-Raw-Measurements/rinex/nav/BRDC00WRD_S_20241810000_01D_MN.rnx")
+        # parser.parse()
+        # ephemeris = parser.get_data()
+        # print(f"Ephemeris data: {ephemeris}")  # Print the first few rows of ephemeris data
+
+        
 
         csv_output = []
         for epoch in measurements['Epoch'].unique():
-            one_epoch = measurements.loc[(measurements['Epoch'] == epoch) & (measurements['prSeconds'] < 0.1)] 
-            one_epoch = one_epoch.drop_duplicates(subset='SvName').set_index('SvName')
+            one_epoch = measurements.loc[(measurements['Epoch'] == epoch)].drop_duplicates(subset='SvName').set_index('SvName')
             if len(one_epoch.index) > 4:
                 timestamp = one_epoch.iloc[0]['UnixTime'].to_pydatetime(warn=False)
+                gps_time = gps_millis[epoch]
 
-                # Calculating satellite positions (ECEF)
                 sats = one_epoch.index.unique().tolist()
-                ephemeris = manager.get_ephemeris(timestamp, sats)
-                sv_position = calculate_satellite_position(ephemeris, one_epoch['tTxSeconds'])
+                rinex_data = RinexNav("C:/Users/shake/OneDrive/Desktop/VsCode/GNSS-Raw-Measurements/rinex/nav/BRDC00WRD_S_20241810000_01D_MN.rnx",sats)
+                rinex_nav_df = rinex_data.pandas_df()
 
-                # Apply satellite clock bias to correct the measured pseudorange values
-                # Ensure sv_position's index matches one_epoch's index
-                sv_position.index = sv_position.index.map(str)  # Ensuring index types match; adjust as needed
-                one_epoch = one_epoch.join(sv_position[['delT_sv']], how='left')
-                one_epoch['PrM_corrected'] = one_epoch['PrM'] + LIGHTSPEED * one_epoch['delT_sv']
-
-                # Doppler shift calculation
-                doppler_calculated = False
-                try:
-                    one_epoch['CarrierFrequencyHz'] = pd.to_numeric(one_epoch['CarrierFrequencyHz'])
-                    one_epoch['DopplerShiftHz'] = -(one_epoch['PseudorangeRateMetersPerSecond'] / LIGHTSPEED) * one_epoch['CarrierFrequencyHz']
-                    doppler_calculated = True
-                except Exception:
-                    pass
-
+                sv_positions = calculate_satellite_position(rinex_nav_df, gps_time)
+                
                 for sv in one_epoch.index:
-                    csv_output.append({
-                        "GPS Time": timestamp.isoformat(),
-                        "SatPRN (ID)": sv,
-                        "Sat.X": sv_position.at[sv, 'x_k'] if sv in sv_position.index else np.nan,
-                        "Sat.Y": sv_position.at[sv, 'y_k'] if sv in sv_position.index else np.nan,
-                        "Sat.Z": sv_position.at[sv, 'z_k'] if sv in sv_position.index else np.nan,
-                        "Pseudo-Range": one_epoch.at[sv, 'PrM_corrected'],
-                        "CN0": one_epoch.at[sv, 'Cn0DbHz'],
-                        "Doppler": one_epoch.at[sv, 'DopplerShiftHz'] if doppler_calculated else 'NaN'
-                    })
+                    id = int(re.search(r'\d+', sv).group())
+
+                    if id in sv_positions['sv_id'].values:
+                        pos = sv_positions[sv_positions['sv_id'] == id].iloc[0]
+                        csv_output.append({
+                            "GPS Time": timestamp.isoformat(),
+                            "SatPRN (ID)": sv,
+                            "Constellation": sv[0],
+                            "Sat.X": pos['x_k'],
+                            "Sat.Y": pos['y_k'],
+                            "Sat.Z": pos['z_k'],
+                            "Pseudo-Range": one_epoch.at[sv, 'PrM'],
+                            "CN0": one_epoch.at[sv, 'Cn0DbHz'],
+                            "Doppler": one_epoch.at[sv, 'DopplerShiftHz'] if 'DopplerShiftHz' in one_epoch.columns else 'NaN'
+                        })
 
         csv_df = pd.DataFrame(csv_output)
         csv_df.to_csv("gnss_measurements_output.csv", index=False)
+        print("CSV output generated successfully.")
     except Exception as e:
         print(f"An error occurred while processing {file_path}: {e}")
         traceback.print_exc()
-
 def main():
     args = parse_arguments()
     directory_to_pull = '/storage/emulated/0/Android/data/com.android.gpstest/files/gnss_log/'
     destination = './'
-
-    #clear the data from the directory so we can append the new file
     delete_files_in_directory('/storage/emulated/0/Android/data/com.android.gpstest/files/gnss_log/')
 
     while True:
         pull_and_append_files(directory_to_pull, destination)
-        # time.sleep(1)  # Polling interval, adjust as needed
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     try:
         main()
     except Exception as e:
