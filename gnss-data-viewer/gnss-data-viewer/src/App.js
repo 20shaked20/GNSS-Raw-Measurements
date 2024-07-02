c71f64b87e70a2e76f56ba232142dae803795a99
20shaked20 <shakedlevi32@gmail.com>
2024-07-02 16:05:12 +0300

    Minor change to ui

--- gnss-data-viewer/gnss-data-viewer/src/App.js
+++ gnss-data-viewer/gnss-data-viewer/src/App.js
@@ -1,14 +1,30 @@
-// src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-// import CSVReaderComponent from './components/CSVReader';
+import CSVReaderComponent from './components/CSVReader';
+import SatelliteView from './components/SatelliteView';
 import LogFileSelectorComponent from './components/LogFileSelector';
 
 function App() {
+  const [currentView, setCurrentView] = useState('GNSS_DATA_VIEWER');
+
+  const handleViewChange = (view) => {
+    setCurrentView(view);
+  };
+
   return (
     <div className="App">
-      {/* <CSVReaderComponent /> */}
-      <LogFileSelectorComponent />
+      <header>
+        <nav>
+          <ul>
+            <li onClick={() => handleViewChange('GNSS_DATA_VIEWER')}>GNSS Data Viewer</li>
+            <li onClick={() => handleViewChange('SAT_VIEW')}>Sat View</li>
+            <li onClick={() => handleViewChange('LOG_FILE_SELECTOR')}>Log File Selector</li>
+          </ul>
+        </nav>
+      </header>
+      {currentView === 'GNSS_DATA_VIEWER' && <CSVReaderComponent />}
+      {currentView === 'SAT_VIEW' && <SatelliteView />}
+      {currentView === 'LOG_FILE_SELECTOR' && <LogFileSelectorComponent />}
     </div>
   );
 }
